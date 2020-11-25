ebba68b6cdc69cbbfcb4b45f38ac800887e1169b
Ui-Hyeop Shin <41767172+dmlguq456@users.noreply.github.com>
2020-11-25 11:26:15 +0900

    Initial commit

---
+++ main.m
@@ -0,0 +1,20 @@
+clc; clear; close all;
+% Performing Blind Source Separation using Independent Vector Analysis(IVA)
+
+[x, Fs] = audioread('x_2x2_instant.wav');
+
+% Parameters
+MaxIter       = 200;
+step_size     = 0.1;
+
+y = ICA(x, MaxIter, step_size);
+
+figure;
+spectrogram(x(:,1),512,384,512,Fs,'yaxis'); colormap jet; caxis([-140 -30]);
+figure;
+spectrogram(y(1,:),512,384,512,Fs,'yaxis');colormap jet;caxis([-140  -30]);
+figure;
+spectrogram(y(2,:),512,384,512,Fs,'yaxis');colormap jet;caxis([-140  -30]);
+
+audiowrite('source1_ICA.wav',y(1,:),Fs);
+audiowrite('source2_ICA.wav',y(2,:),Fs);
